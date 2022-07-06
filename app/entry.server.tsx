7037d59dfedfade7bc253ceea3bc7f23545dd80b
Jacob Ebey <jacob.ebey@live.com>
2022-07-06 11:31:24 -0700

    added heap log

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,13 +1,15 @@
+import { heapStats } from "bun:jsc"
 import type * as RemixServer from "@remix-run/server-runtime";
 import * as RemixReact from "@remix-run/react";
 import { renderToString } from "react-dom/server";
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: RemixServer.EntryContext
 ) {
+  console.log(heapStats());
   let markup = renderToString(
     <RemixReact.RemixServer context={remixContext} url={request.url} />
   );
